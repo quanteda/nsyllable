34ae4c9340aadf0ec93612b040048ebfac7cc0ab
kbenoit <kbenoit@lse.ac.uk>
2020-11-11 09:53:04 +0000

    Add package documentation

---
+++ R/nsyllable-package.R
@@ -0,0 +1,18 @@
+#' nsyllable: Count syllables in character vectors
+#' 
+#' Counts syllables from character vector inputs.  Imputes syllables as the
+#' number of vowel sequences for words not found.
+#' @author Kenneth Benoit
+#' @name nsyllable-package
+#' @seealso 
+#' Useful links:
+#' * <https://github.com/quanteda/nsyllable>
+#' * Report bugs at <https://github.com/quanteda/nsyllable/issues>
+#' @docType package
+NULL
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
