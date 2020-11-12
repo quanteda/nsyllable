1c46c8b69a4fbd6c4e4620b20f9403dff3fcb2c4
kbenoit <kbenoit@lse.ac.uk>
2020-11-12 08:14:14 +0000

    Fix encoding issue, sort, rename

--- data/sources/create_English_CMU.R
+++ data/sources/create_English_CMU.R
@@ -6,20 +6,26 @@ rl <- readLines("https://raw.githubusercontent.com/Alexir/CMUdict/master/cmudict
 # remove comments and entries starting with punctuation
 rl <- rl[!stringi::stri_detect_regex(rl, "^[\\p{P}\\p{S}]")]
 
+# fix one entry for "déjà"
+rl[35609] <- "DÉJÀ  D EY2 JH AA1"
+
 # make each row a list element
 rls <- strsplit(rl, " ")
 # name the element with its first element
-names(rls) <- stri_trans_tolower(sapply(rls, function(x) x[1]))
+names(rls) <- stringi::stri_trans_tolower(sapply(rls, function(x) x[1]))
 # remove first element and spaces
 rls <- lapply(rls, function(x) x[-1])
 # remove variants
-rls <- rls[!stri_detect_fixed(names(rls), "(")]
+rls <- rls[!stringi::stri_detect_fixed(names(rls), "(")]
+
+# sort
+rls <- rls[sort(names(rls))]
 
 # convert to tokens
 toks <- as.tokens(rls)
 # select the phonemes with stresses
 toks <- tokens_select(toks, "\\p{N}$", valuetype = "regex")
 
-data_int_syllables <- ntoken(toks)
+data_syllables_en <- ntoken(toks)
 
-usethis::use_data(data_int_syllables, overwrite = TRUE)
+usethis::use_data(data_syllables_en, overwrite = TRUE)
