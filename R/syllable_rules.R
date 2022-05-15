379375cdca813b35da62bfe702bddb71353856ec
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-15 13:49:05 +0200

    Define rules for counting in Polish

--- R/syllable_rules.R
+++ R/syllable_rules.R
@@ -1,5 +1,6 @@
 apply_syllable_rules <- function(x, language) {
     switch(language,
+           pl = apply_pl_rules(x),
            apply_default_rules(x)
     )
 }
@@ -7,6 +8,19 @@ apply_syllable_rules <- function(x, language) {
 apply_default_rules <- function(x) {
     vapply(
         gregexpr("[aeiouy]+", x, ignore.case = TRUE),
+        function(match) {
+            if (length(match) == 1 && match == -1)
+                NA_integer_
+            else
+                length(match)
+        },
+        integer(1)
+    )
+}
+
+apply_pl_rules <- function(x) {
+    vapply(
+        gregexpr("i*([aeiouy])\\1*u*", x, ignore.case = TRUE),
         function(match) {
             if (length(match) == 1 && match == -1)
                 NA_integer_
